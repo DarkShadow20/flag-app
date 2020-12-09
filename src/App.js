0af7547df57bd2a76d40bc9f5e2e2fa46952c4ef
DarkShadow20 <kunal8011@gmail.com>
2020-12-09 14:56:54 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,208 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+var FlagDictionary = {
+  "🇦🇨": "Ascension Island",
+  "🇦🇩": "Andorra",
+  "🇦🇪": "United Arab Emirates",
+  "🇦🇫": "Afghanistan",
+  "🇦🇬": "Antigua & Barbuda",
+  "🇦🇮": "Anguilla",
+  "🇦🇱": "Albania",
+  "🇦🇲": "Armenia",
+  "🇦🇴": "Angola",
+  "🇦🇶": "Antarctica",
+  "🇦🇷": "Argentina",
+  "🇦🇸": "American Samoa",
+  "🇦🇹": "Austria",
+  "🇦🇺": "Australia",
+  "🇦🇼": "Aruba",
+  "🇦🇽": "Åland Islands",
+  "🇦🇿": "Azerbaijan",
+  "🇧🇦": "Bosnia & Herzegovina",
+  "🇧🇧": "Barbados",
+  "🇧🇩": "Bangladesh",
+  "🇧🇪": "Belgium",
+  "🇧🇫": "Burkina Faso",
+  "🇧🇬": "Bulgaria",
+  "🇧🇭": "Bahrain",
+  "🇧🇮": "Burundi",
+  "🇧🇯": "Benin",
+  "🇧🇱": "St. Barthélemy",
+  "🇧🇲": "Bermuda",
+  "🇧🇳": "Brunei",
+  "🇧🇴": "Bolivia",
+  "🇧🇶": "Caribbean Netherlands",
+  "🇧🇷": "Brazil",
+  "🇧🇸": "Bahamas",
+  "🇧🇹": "Bhutan",
+  "🇧🇻": "Bouvet Island",
+  "🇧🇼": "Botswana",
+  "🇧🇾": "Belarus",
+  "🇧🇿": "Belize",
+  "🇨🇦": "Canada",
+  "🇨🇨": "Cocos (Keeling) Islands",
+  "🇨🇰": "Cook Islands",
+  "🇨🇱": "Chile",
+  "🇨🇲": "Cameroon",
+  "🇨🇳": "China",
+  "🇨🇴": "Colombia",
+  "🇨🇵": "Clipperton Island",
+  "🇨🇷": "Costa Rica",
+  "🇨🇺": "Cuba",
+  "🇨🇻": "Cape Verde",
+  "🇨🇼": "Curaçao",
+  "🇨🇽": "Christmas Island",
+  "🇨🇾": "Cyprus",
+  "🇨🇿": "Czechia",
+  "🇩🇪": "Germany",
+  "🇩🇬": "Diego Garcia",
+  "🇩🇯": "Djibouti",
+  "🇩🇰": "Denmark",
+  "🇩🇲": "Dominica",
+  "🇩🇴": "Dominican Republic",
+  "🇩🇿": "Algeria",
+  "🇪🇦": "Ceuta & Melilla",
+  "🇪🇨": "Ecuador",
+  "🇪🇪": "Estonia",
+  "🇪🇬": "Egypt",
+  "🇪🇭": "Western Sahara",
+  "🇪🇷": "Eritrea",
+  "🇪🇸": "Spain",
+  "🇪🇹": "Ethiopia",
+  "🇪🇺": "European Union",
+  "🇫🇮": "Finland",
+  "🇫🇯": "Fiji",
+  "🇫🇰": "Falkland Islands",
+  "🇫🇲": "Micronesia",
+  "🇫🇴": "Faroe Islands",
+  "🇫🇷": "France",
+  "🇬🇦": "Gabon",
+  "🇬🇧": "United Kingdom",
+  "🇬🇵": "Guadeloupe",
+  "🇬🇶": "Equatorial Guinea",
+  "🇬🇷": "Greece",
+  "🇬🇸": "South Georgia & South Sandwich Islands",
+  "🇭🇺": "Hungary",
+  "🇮🇨": "Canary Islands",
+  "🇮🇩": "Indonesia",
+  "🇮🇱": "Israel",
+  "🇮🇳": "India",
+  "🇮🇴": "British Indian Ocean Territory",
+  "🇮🇶": "Iraq",
+  "🇮🇷": "Iran",
+  "🇮🇸": "Iceland",
+  "🇮🇹": "Italy",
+  "🇯🇪": "Jersey",
+  "🇯🇲": "Jamaica",
+  "🇯🇴": "Jordan",
+  "🇯🇵": "Japan",
+  "🇰🇪": "Kenya",
+  "🇰🇬": "Kyrgyzstan",
+  "🇰🇭": "Cambodia",
+  "🇰🇮": "Kiribati",
+  "🇰🇲": "Comoros",
+  "🇱🇾": "Libya",
+  "🇲🇦": "Morocco",
+  "🇲🇱": "Mali",
+  "🇲🇲": "Myanmar (Burma)",
+  "🇲🇵": "Northern Mariana Islands",
+  "🇲🇶": "Martinique",
+  "🇲🇷": "Mauritania",
+  "🇲🇸": "Montserrat",
+  "🇲🇹": "Malta",
+  "🇲🇺": "Mauritius",
+  "🇲🇻": "Maldives",
+  "🇲🇼": "Malawi",
+  "🇲🇽": "Mexico",
+  "🇲🇾": "Malaysia",
+  "🇳🇱": "Netherlands",
+  "🇳🇴": "Norway",
+  "🇳🇵": "Nepal",
+  "🇳🇷": "Nauru",
+  "🇳🇺": "Niue",
+  "🇳🇿": "New Zealand",
+  "🇴🇲": "Oman",
+  "🇵🇦": "Panama",
+  "🇵🇪": "Peru",
+  "🇵🇫": "French Polynesia",
+  "🇵🇬": "Papua New Guinea",
+  "🇵🇭": "Philippines",
+  "🇷🇺": "Russia",
+  "🇷🇼": "Rwanda",
+  "🇸🇦": "Saudi Arabia",
+  "🇸🇧": "Solomon Islands",
+  "🇸🇨": "Seychelles",
+  "🇸🇩": "Sudan",
+  "🇸🇪": "Sweden",
+  "🇸🇬": "Singapore",
+  "🇸🇿": "Eswatini",
+  "🇹🇦": "Tristan Da Cunha",
+  "🇹🇨": "Turks & Caicos Islands",
+  "🇹🇩": "Chad",
+  "🇹🇫": "French Southern Territories",
+  "🇹🇬": "Togo",
+  "🇹🇭": "Thailand",
+  "🇹🇯": "Tajikistan",
+  "🇹🇰": "Tokelau",
+  "🇹🇱": "Timor-Leste",
+  "🇹🇲": "Turkmenistan",
+  "🇹🇳": "Tunisia",
+  "🇹🇴": "Tonga",
+  "🇹🇷": "Turkey",
+  "🇻🇦": "Vatican City",
+  "🇻🇨": "St. Vincent & Grenadines",
+  "🇻🇪": "Venezuela",
+  "🇻🇬": "British Virgin Islands",
+  "🇻🇮": "U.S. Virgin Islands",
+  "🇿🇲": "Zambia",
+  "🇿🇼": "Zimbabwe"
+};
+var FlagArray = Object.keys(FlagDictionary);
+export default function App() {
+  const [meaning, setMeaning] = useState("");
+  function flagInputHandler() {
+    var userInput = event.target.value;
+    var meaning = FlagDictionary[userInput];
+
+    if (meaning === undefined) {
+      meaning = "Sorry! try again";
+    }
+    setMeaning(meaning);
+  }
+  function flagClickHandler(flag) {
+    setMeaning(FlagDictionary[flag]);
+  }
+  return (
+    <div className="App">
+      <h1>
+        <span role="img" aria-label="flag">
+          🏁
+        </span>
+        Fun with Flags
+      </h1>
+      <input
+        onChange={flagInputHandler}
+        placeholder="place your flag emoji here"
+      ></input>
+      <p>Click on these flags to know there names</p>
+      <div>{meaning}</div>
+      <p>Currently we know these flags</p>
+      <div className="flag">
+        {FlagArray.map((Flags) => {
+          return (
+            <span
+              role="img"
+              aria-label={Flags}
+              className="flagmaker"
+              onClick={() => flagClickHandler(Flags)}
+              key={Flags}
+            >
+              {Flags}
+            </span>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
